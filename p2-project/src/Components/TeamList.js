445b5c4e0929215da5b0562f919b8b863be48e99
devinheinzler <devinheinzler271@gmail.com>
2022-10-24 14:54:03 -0500

    added styling for list

--- p2-project/src/Components/TeamList.js
+++ p2-project/src/Components/TeamList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import TeamCard from "./TeamCard"
+import './TeamList.css'
 
 const teamsUrl = `http://localhost:3001/teams`
 
@@ -16,7 +17,9 @@ useEffect(() => {
     return (
         teams.map(team => 
             <div className="card-list" key={team.id}>
-                <li>{team.name} ({team.short_code})</li>
+                <ul>
+                    <li>{team.name} ({team.short_code})</li>
+                </ul>
             </div>
             )
     )
