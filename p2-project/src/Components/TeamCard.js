248141ffd405479543e6ce97e283442650c1f94a
devinheinzler <devinheinzler271@gmail.com>
2022-10-28 11:49:07 -0500

    added some card styling and buttons

--- p2-project/src/Components/TeamCard.js
+++ p2-project/src/Components/TeamCard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { useParams, Link } from "react-router-dom"
 import constants from "./Constants.js"
-import { Header, Button, Icon } from 'semantic-ui-react'
+import { Header, Button, Icon, Card, Image } from 'semantic-ui-react'
 
 const teamsUrl = constants.teamsUrl
 const playersUrl = constants.playersUrl + "?team_id="
@@ -61,17 +61,27 @@ function TeamCard(props) {
     return ( 
         
         <div className="card" key={team.id}>
-            <div className="side-by-side">
-                
-                <h2>{team.name} ({team.short_code})</h2>
+
+            <Card>
+                <Image src={team.logo_path} wrapped ui={false} />
+                <Card.Content>
+                <Card.Header>{team.name} ({team.short_code})</Card.Header>
+                <Card.Meta>
+                    <span className='founded'>Founded in: {team.founded}</span>
+                </Card.Meta>
+                <Card.Meta>
+                    {team.twitter}
+                </Card.Meta>
                 <button onClick={() => props.handleFavoriteTeamClicked(team.id)}>{isFavoriteTeam(team.id)}</button>
-            </div>
-            <h3>{team.twitter}</h3>
-            <img src={team.logo_path} alt={team.name} />
-            <h4>
-                Founded in: {team.founded}
-            </h4>
-            <h4> Roster </h4>
+                </Card.Content>
+                <Card.Content extra>
+                <a>
+                    <Icon name='user' />
+                    {getPlayers().length} players
+                </a>
+                </Card.Content>
+            </Card>
+            <h4> Roster: </h4>
             <form>
                 <label for="filter">Filter Players: </label>
                 <input name="filter" onChange={handleOnChange}></input>
@@ -79,10 +89,17 @@ function TeamCard(props) {
             <br></br>
             <th>
                 {getPlayers().map(p=>{
-                    return <tr><td>{p.name}</td><td><button onClick={() => props.handleFavoriteClick(p.player_id, p.name, team.name)}>{isFavorite(p.player_id)}</button></td></tr>})}
+                    return <tr><td>{p.name}</td><td>
+                        <button animated onClick={() => props.handleFavoriteClick(p.player_id, p.name, team.name)}>{isFavorite(p.player_id)}</button>
+                        </td></tr>
+                    })}
             </th>
             <br/>
+            <Button icon labelPosition='left'>
             <Link to="/">Back To Teams</Link>
+                <Icon name='left arrow' />
+            </Button>
+            {/* <Link to="/">Back To Teams</Link> */}
         </div>
     )
 }
