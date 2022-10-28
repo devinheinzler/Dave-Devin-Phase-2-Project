2d0404f5fd403e9986f25a502869b551f4ae6353
David <david.v.bernardo@gmail.com>
2022-10-27 20:38:43 -0500

    break data in two

--- p2-project/src/Components/Constants.js
+++ p2-project/src/Components/Constants.js
@@ -7,7 +7,8 @@ const constants = {
     favoritePlayersUrl: 'http://localhost:3002/favoritePlayers',
     playersUrl: 'http://localhost:3001/players',
     teamsUrl: `http://localhost:3001/teams`,
-    shortTeamsUrl: 'http://localhost:3001/short_teams'
+    shortTeamsUrl: 'http://localhost:3001/short_teams',
+    favoriteTeamsUrl: 'http://localhost:3002/favoriteTeams'
 }
 
 export default constants
--- p2-project/src/Components/ScoreScroll.js
+++ p2-project/src/Components/ScoreScroll.js
@@ -33,7 +33,6 @@ const ScoreScroll = (props) => {
       const getScoreText = () => {
         if (scores.length == 0) return "Call To Live Scores Failed"
         let text = ""
-        console.log(teams)
         scores.forEach(s => {
           let homeTeamName = s.localteam_id
           let homeCountry = "?"
